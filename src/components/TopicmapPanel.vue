d53b3c7873ff677464eaf386666526494159852c
Jörg Richter <jri@deepamehta.de>
2017-03-25 15:30:43 +0100

    Basal flex layout

---
+++ src/components/TopicmapPanel.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="topicmap-panel"></div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style lang="stylus">
+#topicmap-panel
+  background-color #ffe
+</style>
